7f0553610430f2496b0cf8f21279935b4fafa095
VitorVolpon <vitor.volpon@gmail.com>
2025-02-24 17:09:10 -0300

    commit

--- dash_car_sale.py
+++ dash_car_sale.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 # Mostrar qual mês teve o maior número de vendas
 # um gráfico de linhas com o total de vendas em cada mes
 # gráfico de barras horizontal com os modelos de cara marca e qual vendeu mais (maior ao menor)
-#Filtros de ano, tipos de carro, tipo de cambio, genero, cor (com opção de All)
+# Filtros de ano, tipos de carro, tipo de cambio, genero, cor (com opção de All)
 # talvez um de pizza por porcentagem de cor de carros mais vendidas
 # em cima quantidate total de vendas, preço médio de carro, total de vendas em $, e total de carros automatico e manuais(número total e do lado a porcentagem entre os dois)
 
@@ -36,7 +36,7 @@ filter_2023 = st.sidebar.checkbox("2023", value=filter_all, disabled=filter_all)
 
 
 # Aplicar o filtro no dataframe com base na seleção dos checkboxes
-if filter_all or (not filter_2022 and not filter_2023):  # Se "All" for selecionado ou nenhum outro ano for marcado
+if filter_all or (not filter_2022 and not filter_2023):  
     df_filtered = df
 else:
     selected_years = []
@@ -47,7 +47,6 @@ else:
     
     df_filtered = df[df["Year"].isin(selected_years)]
 
-# Exibir o dataframe filtrado
 df_filtered
 
 col1, col2, col3 = st.columns(3)
@@ -58,9 +57,9 @@ month_total["Month"] = pd.to_datetime(month_total["Month"])
 month_total = month_total.sort_values("Month")
 fig_line = px.line(month_total, x="Month", y="Price ($)"   
                    , title="Venda de Carros por Mês", markers=True)
-fig_line.update_traces(line=dict(color="rgba(255, 0, 0, 0.5)"))  # Vermelho com 70% de opacidade
+fig_line.update_traces(line=dict(color="rgba(255, 0, 0, 0.5)")) 
 fig_line.update_traces(marker=dict(color="rgba(255, 0, 0, 0.5)", size=8))
-fig_line.update_traces(fill="tozeroy", fillcolor="rgba(0, 0, 255, 0.2)")  # Azul com transparência
+fig_line.update_traces(fill="tozeroy", fillcolor="rgba(0, 0, 255, 0.2)") 
 col1.plotly_chart(fig_line, use_container_width=True)
 
 car_colors = df.groupby("Color")[["Car_id"]].count().reset_index()
